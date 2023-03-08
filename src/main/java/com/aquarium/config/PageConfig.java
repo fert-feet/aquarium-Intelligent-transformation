e28b90446995b09fab82e392c95dfba625e26401
ke2fy <3214588281@qq.com>
2023-03-08 17:39:21 +0800

    添加分页拦截器

---
+++ src/main/java/com/aquarium/config/PageConfig.java
@@ -0,0 +1,25 @@
+package com.aquarium.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.aquarium.mapper")
+public class PageConfig {
+
+    /**
+     * 分页插件
+     *
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
